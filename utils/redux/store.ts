b43066df98e1537bc5850fb3bbe6ca443106f000
Kshitij Srivastava <kshitij.suraj@gmail.com>
2024-09-09 18:52:58 +0530

    feat: Update fonts and add ReduxProvider to RootLayout

---
+++ utils/redux/store.ts
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+
+const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false
+      // serializableCheck: {
+      //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      // },
+    })
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch<AppDispatch>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export default store
